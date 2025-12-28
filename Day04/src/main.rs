f05194cc8b08331d290181bac929fa26f8dcbbe6
Nighthawk <luks.schroeter@gmail.com>
2025-12-28 12:15:35 +0100

    Pt1

---
+++ Day04/src/main.rs
@@ -0,0 +1,72 @@
+use std::{collections::HashMap, fs::read_to_string};
+
+fn main() {
+    let input = read_to_string("demo.txt").unwrap();
+    
+    let feld:Vec<Vec<char>> = input.lines().map(|x| x.trim().chars().collect()).collect();
+    println!("");
+
+    // Part 1
+
+    let mut sum=0;
+    for i in 0..feld.len(){
+
+        for j in 0..feld[i].len(){
+
+            if feld[i][j] =='.'{
+                sum+=checkrolls(j, i, &feld);
+                println!("y {} x {}", j,i);
+
+        }
+    }
+
+   }
+
+   println!("{}", sum);
+}
+ 
+ fn checkrolls(posx:usize, posy:usize, map:&Vec<Vec<char>>)-> i32{
+
+
+    // y x 
+    let array = [[-1, 0], [1,0], [-1,-1], [-1, 1], [0,1], [0,-1], [1, -1], [1, 1]];
+
+
+    let x = posx as i32;
+    let y = posy as i32;
+    let mut sum=0;
+    for i in 0..array.len(){
+        let yn = y + array[i][0];
+        let xn = x + array[i][1];
+
+        if 0<= xn && 0<= yn && yn < map.len() as i32 && xn < map[0].len() as i32  {
+
+            if map[yn as usize][xn as usize] != '@' {
+                sum+=1;
+                
+            }
+            
+        }
+        else {
+            sum+=1;
+        }
+    }
+
+    if sum <= 6{
+        return 1;
+        
+    }
+    else {
+        return 0;
+    }
+
+    
+
+ }
+
+
+ /*
+ 1 2 3
+ 4 X 5
+ 6 7 8
+  */
